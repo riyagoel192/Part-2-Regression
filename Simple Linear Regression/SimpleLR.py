5405c90e5f48be054655e1b356e02059d7fa6883
Riya Goel <55539590+riyagoel192@users.noreply.github.com>
2020-03-29 22:41:35 +0530

    Add files via upload

---
+++ Simple Linear Regression/SimpleLR.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 28 17:49:31 2020
+
+@author: Aditya
+"""
+
+#Simple Linear Regression
+
+import numpy as np
+import pandas as pd     
+import matplotlib.pyplot as plt   
+
+#importing the dataset
+dataset= pd.read_csv("Salary_Data.csv")
+X= dataset.iloc[:, :-1].values        #array of predictor variables i.e removing last col
+Y= dataset.iloc[:, 1].values          #target output variable array as indexing start at 0
+
+#Splitting the dataset into training and test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 1/3, random_state = 0)
+
+#Fitting Simple Linear Regression to the Training Set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, Y_train)
+
+#Predicting the test set results
+y_pred = regressor.predict(X_test)  #Y_test contains real salaries and y_pred contains predicted salaries
+
+#Visualising the Training set results
+plt.scatter(X_train,Y_train, color='red')
+plt.plot(X_train,regressor.predict(X_train),color='blue')
+plt.title('Salary vs Experience(Training Set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+
+#Visualising the Test set results
+plt.scatter(X_test,Y_test, color='red')
+plt.plot(X_train,regressor.predict(X_train),color='blue')
+plt.title('Salary vs Experience(Test Set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
